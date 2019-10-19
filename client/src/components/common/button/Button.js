6f024226563e7e04dd9a3e0ff4c5e9a1a5cfade9
Paweł Bujna <pawelbujna@gmail.com>
2019-10-19 17:04:10 +0200

    Fixed propTypes in button

--- client/src/components/common/button/Button.js
+++ client/src/components/common/button/Button.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 function Button (props) {
   return (
     <button>{props.text}</button>
   )
+}
+
+Button.propTypes = {
+  text: PropTypes.string
 }
 
 export default Button
